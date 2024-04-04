1a4a13c88053ea691a46e64078ac7ba898b43d4b
DESKTOP-EDISL93\gravi <zeal22300050@fsgcl.jp>
2024-04-04 15:33:44 +0900

    カメラの基礎部分を記述

---
+++ Project/GameSources/MainCamera.cpp
@@ -0,0 +1,18 @@
+/*!
+@file MainCamera.cpp
+@brief ƒJƒƒ‰
+@prod –î—I—t
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	void MainCamera::OnCreate() {
+
+	}
+
+	void MainCamera::OnUpdate() {
+
+	}
+}
--- Project/GameSources/MainCamera.h
+++ Project/GameSources/MainCamera.h
@@ -1,1 +1,37 @@
+/*!
+@file MainCamera.h
+@brief カメラ
+@prod 矢吹悠葉
+*/
+
 #pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class MainCamera : public Camera {
+		weak_ptr<GameObject> m_targetObject;	// 目標となるオブジェクト
+		shared_ptr<Transform> m_targetTrans;	// 目標のトランスフォーム
+
+	public:
+		// コンストラクタとデストラクタ
+		MainCamera() : Camera()
+		{
+		}
+		~MainCamera() {}
+
+		void OnCreate() override;
+		void OnUpdate() override;
+
+		// カメラが追尾するオブジェクトを取得する関数
+		shared_ptr<GameObject> GetTargetObject() const {
+			if (!m_targetObject.expired()) {
+				return m_targetObject.lock();
+			}
+			return nullptr;
+		}
+		// カメラが追尾するオブジェクトを設定する関数
+		void SetTargetObject(const shared_ptr<GameObject>& Obj) {
+			m_targetObject = Obj;
+		}
+	};
+}
