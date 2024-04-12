3ea5662562f7b13c06c5a913b22545e70c132d2a
Aoi Niitsuma <zeal22300055@fsjcl.jp>
2024-04-12 14:45:07 +0900

    Aボタン押すと、フェード後にゲームステージに移行する

--- Project/GameSources/Building.cpp
+++ Project/GameSources/Building.cpp
@@ -28,7 +28,8 @@ namespace basecross {
 
 	void Company::OnUpdate()
 	{
-		if (m_pushButton)
+		auto& a = GetStage()->GetSharedGameObject<CompanyCollision>(L"COMPANYCOLL");
+		if (a->GetPlayerHitFlag())
 		{
 			ButtonPush();
 		}
@@ -72,18 +73,7 @@ namespace basecross {
 
 		auto CollComp = AddComponent<CollisionObb>();
 		CollComp->SetDrawActive(true);
-	}
-
-	void CompanyCollision::OnCollisionEnter(shared_ptr<GameObject>& object)
-	{
-		if (object->FindTag(L"Player"))
-		{
-			isPushButton = true;
-		}
-		else
-		{
-			isPushButton = false;
-		}
+		CollComp->SetFixed(true);
 	}
 
 	void Construction::OnCreate()
@@ -98,26 +88,36 @@ namespace basecross {
 		drawComp->SetMeshToTransformMatrix(m_spanMat);
 		drawComp->SetMeshResource(L"CONSTRUCTION");
 		drawComp->SetDiffuse(COL_YELOW);
+
+		const float width = static_cast<float>(App::GetApp()->GetGameWidth());
+		const float height = static_cast<float>(App::GetApp()->GetGameHeight());
+
+		m_fadeSprite = GetStage()->AddGameObject<Sprite>(L"FADE_TX", Vec2(width, height), Vec3(0.0f));
+		m_fadeSprite->SetDiffuseColor(COL_ALPHA);
 	}
 
 	void Construction::OnUpdate()
 	{
 		StartButtonPush();
+		StartCountDown();
 	}
 
 	void Construction::StartButtonPush()
 	{
-		if (Input::GetPushA())
+		if (Input::GetPushA() && !isPushButton)
+		{
+			isPushButton = true;
+		}
+	}
+
+	void Construction::StartCountDown()
+	{
+		if (isPushButton && m_fadeSprite->FadeInColor(3.0f))
 		{
 			// ステージ変更のため、シーンを取得
 			auto& scene = App::GetApp()->GetScene<Scene>();
 
 			PostEvent(0.0f, GetThis<ObjectInterface>(), scene, L"GameStage");
 		}
-	}
-
-	void Construction::Start()
-	{
-
 	}
 }
