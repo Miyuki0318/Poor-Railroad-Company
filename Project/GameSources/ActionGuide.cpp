a7ede56eb62cd39fa9c3e3cd2460b08c6107f2fa
Hiroki Ozawa <zeal22300043@fsgcl.jp>
2024-04-16 13:47:26 +0900

    コメントと関数分け

--- Project/GameSources/ActionGuide.cpp
+++ Project/GameSources/ActionGuide.cpp
@@ -1,57 +1,81 @@
+/*!
+@file ActionGuide.cpp
+@brief 行動のガイドアイコン描画
+@author 小澤博貴
+*/
+
 #include "stdafx.h"
 #include "Project.h"
 
 namespace basecross
 {
+	// 生成時の処理を実行
 	void ActionGuide::OnCreate()
 	{
+		// 継承元の生成時の処理を実行
 		Billboard::OnCreate();
+
+		// テクスチャを変更
 		SetTexture(m_texMap.at(m_iconType));
+
+		// 非表示に
 		SetDrawActive(false);
 	}
 
+	// 毎フレーム更新処理
 	void ActionGuide::OnUpdate()
 	{
+		// ビルボードと座標の更新
 		UpdateBillboard();
+		UpdatePosition();
 
+		// アイコンタイプの更新
+		m_iconType = eActionIcon::None;
+		eActionIcon icon = eActionIcon::None;
+		UpdateIconType();
+
+		// 変更があればテクスチャの変更
+		if (icon != m_iconType) SetTexture(m_texMap.at(m_iconType));
+
+		// 表示非表示切り替え
+		SetDrawActive(m_iconType != eActionIcon::None);
+	}
+
+	// 座標の更新
+	void ActionGuide::UpdatePosition()
+	{
+		// プレイヤーが存在するかのエラーチェック
+		const auto& player = m_player.lock();
+		if (!player) return;
+
+		// プレイヤーの座標+頭上分の座標
+		SetPosition(player->GetPosition() + m_diffPosition);
+	}
+
+	// アイコンタイプの更新
+	void ActionGuide::UpdateIconType()
+	{
 		// プレイヤーとインディケーターが存在するか
 		const auto& player = m_player.lock();
 		const auto& indicator = m_indicator.lock();
 		if (!player || !indicator) return;
 
-		m_iconType = eActionIcon::None;
-		eActionIcon icon = m_iconType;
-		SetPosition(player->GetPosition() + m_diffPosition);
+		// クラフト可能ならクラフトアイコン
+		if (player->GetCraftPosshible()) m_iconType = eActionIcon::Craft;
 
-		if (player->GetCraftPosshible())
-		{
-			m_iconType = eActionIcon::Craft;
-		}
-
+		// ステージcsvIDに応じて設定
 		const auto& stageMap = GetStage()->GetSharedGameObject<RailManager>(L"RailManager")->GetGuideMap();
 		const auto& selectPoint = indicator->GetSelectPoint();
 		if (Utility::WithInElemRange(selectPoint.x, selectPoint.y, stageMap))
 		{
 			eStageID id = static_cast<eStageID>(stageMap.at(selectPoint.x).at(selectPoint.y));
-			if (id == eStageID::Tree)
+			m_iconType = m_iconMap.at(id);
+
+			// レールを未所持ならNoneに
+			if (m_iconType == eActionIcon::Rail && !player->GetStatus(ePlayerStatus::IsHaveRail))
 			{
-				m_iconType = eActionIcon::Axe;
-			}
-			if (id == eStageID::Rock)
-			{
-				m_iconType = eActionIcon::Pick;
-			}
-			if (id == eStageID::GuideRail && player->GetStatus(ePlayerStatus::IsHaveRail))
-			{
-				m_iconType = eActionIcon::Rail;
+				m_iconType = eActionIcon::None;
 			}
 		}
-
-		if (icon != m_iconType)
-		{
-			SetTexture(m_texMap.at(m_iconType));
-		}
-
-		SetDrawActive(m_iconType != eActionIcon::None);
 	}
 }
--- Project/GameSources/ActionGuide.h
+++ Project/GameSources/ActionGuide.h
@@ -1,9 +1,16 @@
+/*!
+@file ActionGuide.h
+@brief 行動のガイドアイコン描画
+@author 小澤博貴
+*/
+
 #pragma once
 #include "Billboard.h"
 #include "GamePlayer.h"
 
 namespace basecross
 {
+	// アイコンタイプenum
 	enum class eActionIcon : size_t
 	{
 		None,	// 何も無し
@@ -13,16 +20,27 @@ namespace basecross
 		Rail,	// レール
 	};
 
+	/*!
+	@brief 行動ガイドアイコン
+	*/
 	class ActionGuide : public Billboard
 	{
-		eActionIcon m_iconType;
-		map<eActionIcon, wstring> m_texMap;
-		const weak_ptr<GamePlayer> m_player;
-		const weak_ptr<SelectIndicator> m_indicator;
-		const Vec3 m_diffPosition;
+		eActionIcon m_iconType;	// アイコンのタイプ
+		map<eStageID, eActionIcon> m_iconMap; // アイコンテーブル
+		map<eActionIcon, wstring> m_texMap;	// テクスチャテーブル
+
+		const weak_ptr<GamePlayer> m_player; // プレイヤーのポインタ
+		const weak_ptr<SelectIndicator> m_indicator; // インディケーターのポインタ
+		const Vec3 m_diffPosition; // 頭上までの座標のずれ
 
 	public:
 
+		/*!
+		@brief コンストラクタ
+		@param ステージポインタ
+		@param プレイヤーポインタ
+		@param インディケーターポインタ
+		*/
 		ActionGuide(const shared_ptr<Stage>& stagePtr,
 			const shared_ptr<GamePlayer>& playerPtr,
 			const shared_ptr<SelectIndicator>& indicatorPtr
@@ -32,7 +50,11 @@ namespace basecross
 			m_indicator(indicatorPtr),
 			m_diffPosition(Vec3(0.0f, 2.0f, 0.5f))
 		{
-			m_iconType = eActionIcon::Pick;
+			m_iconType = eActionIcon::None;
+
+			m_iconMap.insert(make_pair(eStageID::Rock, eActionIcon::Pick));
+			m_iconMap.insert(make_pair(eStageID::Tree, eActionIcon::Axe));
+			m_iconMap.insert(make_pair(eStageID::GuideRail, eActionIcon::Rail));
 
 			m_texMap.insert(make_pair(eActionIcon::Pick, L"ICON_PICK_TX"));
 			m_texMap.insert(make_pair(eActionIcon::Axe, L"ICON_AXE_TX"));
@@ -40,10 +62,31 @@ namespace basecross
 			m_texMap.insert(make_pair(eActionIcon::Rail, L"ICON_RAIL_TX"));
 		}
 
+		/*!
+		@brief デストラクタ
+		*/
 		virtual ~ActionGuide() {}
 
+		/*!
+		@brief 生成時に一度だけ呼び出される関数
+		*/
 		void OnCreate() override;
 
+		/*!
+		@brief 毎フレーム度に呼び出される関数
+		*/
 		void OnUpdate() override;
+
+	private:
+
+		/*!
+		@brief 座標の更新処理関数
+		*/
+		void UpdatePosition();
+
+		/*!
+		@brief アイコンタイプの更新処理関数
+		*/
+		void UpdateIconType();
 	};
 }
