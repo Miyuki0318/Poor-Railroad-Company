5229828ea01d9ac06e50c6a462b0a0b776035164
Hiroki Ozawa <zeal22300043@fsgcl.jp>
2024-04-19 13:56:29 +0900

    アクションガイドの更新

--- Project/GameSources/ActionGuide.cpp
+++ Project/GameSources/ActionGuide.cpp
@@ -12,30 +12,41 @@ namespace basecross
 	// 生成時の処理を実行
 	void ActionGuide::OnCreate()
 	{
-		// 継承元の生成時の処理を実行
-		Billboard::OnCreate();
+		// スプライトの生成
+		const auto& stagePtr = GetStage();
+		m_spriteMap.push_back(SpriteType(eIconPivot::Center, eSpriteType::Icon,    stagePtr->AddGameObject<Sprite>()));
+		m_spriteMap.push_back(SpriteType(eIconPivot::Center, eSpriteType::Balloon, stagePtr->AddGameObject<Sprite>(L"I_BALOON_CENTER_TX")));
+		m_spriteMap.push_back(SpriteType(eIconPivot::Right,  eSpriteType::Icon,    stagePtr->AddGameObject<Sprite>()));
+		m_spriteMap.push_back(SpriteType(eIconPivot::Right,  eSpriteType::Balloon, stagePtr->AddGameObject<Sprite>(L"I_BALOON_LEFT_TX")));
+		m_spriteMap.push_back(SpriteType(eIconPivot::Left,   eSpriteType::Icon,    stagePtr->AddGameObject<Sprite>()));
+		m_spriteMap.push_back(SpriteType(eIconPivot::Left,   eSpriteType::Balloon, stagePtr->AddGameObject<Sprite>(L"I_BALOON_RIGHT_TX")));
 
-		// 非表示に
+		// スケールの設定
+		for (auto& sprite : m_spriteMap)
+		{
+			sprite.sprite.lock()->SetScale(m_scale);
+		}
+
 		SetDrawActive(false);
 	}
 
 	// 毎フレーム更新処理
 	void ActionGuide::OnUpdate()
 	{
-		// ビルボードと座標の更新
-		UpdateBillboard();
-		UpdatePosition();
+		// アクティブを初期化
+		SetSpriteActive(false);
 
 		// アイコンタイプの更新
-		m_iconType = eActionIcon::None;
-		eActionIcon icon = eActionIcon::None;
 		UpdateIconType();
 
-		// 変更があればテクスチャの変更
-		if (icon != m_iconType) SetTexture(m_texMap.at(m_iconType));
+		// アイコンの描画更新
+		UpdateIconSet();
 
-		// 表示非表示切り替え
-		SetDrawActive(m_iconType != eActionIcon::None);
+		// ビルボードと座標の更新
+		UpdatePosition();
+
+		// 描画の更新
+		UpdateIconDraw();
 	}
 
 	// 座標の更新
@@ -46,44 +57,112 @@ namespace basecross
 		if (!player) return;
 
 		// プレイヤーの座標+頭上分の座標
-		SetPosition(player->GetPosition() + m_diffPosition);
+		Vec3 headPos = player->GetPosition() + m_diffPosition;
+		
+		// 2D座標に変換して座標を更新
+		const auto& view = GetStage()->GetView();
+		m_position = Utility::ConvertToWorldPosition(view, headPos);
+		m_position.z = 0.0f;
 	}
 
 	// アイコンタイプの更新
 	void ActionGuide::UpdateIconType()
 	{
+		// バッファの初期化
+		m_iconBuffer.clear();
+
 		// プレイヤーとインディケーターが存在するか
 		const auto& player = m_player.lock();
 		const auto& indicator = m_indicator.lock();
 		if (!player || !indicator) return;
 
 		// クラフト可能ならクラフトアイコン
-		if (player->GetCraftPosshible()) m_iconType = eActionIcon::Craft;
+		if (player->GetCraftPosshible()) m_iconBuffer.insert(make_pair(m_texMap.at(eActionIcon::Craft).layerIndex, eActionIcon::Craft));
 
 		// ステージcsvIDに応じて設定
 		const auto& stageMap = GetStage()->GetSharedGameObject<RailManager>(L"RailManager")->GetGuideMap();
 		const auto& selectPoint = indicator->GetSelectPoint();
-		if (Utility::WithInElemRange(selectPoint.x, selectPoint.y, stageMap))
-		{
-			// IDに応じてアイコンタイプを設定
-			eStageID id = static_cast<eStageID>(stageMap.at(selectPoint.x).at(selectPoint.y));
-			if (m_iconMap.find(id) != m_iconMap.end()) m_iconType = m_iconMap.at(id);
+		if (!Utility::WithInElemRange(selectPoint.x, selectPoint.y, stageMap)) return;
 
-			// レールを未所持ならNoneに
-			if (m_iconType == eActionIcon::Rail)
+		// IDに応じてアイコンタイプを設定
+		eStageID id = STAGE_ID(stageMap.at(selectPoint.x).at(selectPoint.y));
+
+		// ガイドレールかの真偽と採掘オブジェクトかの真偽
+		bool isGuide = (id == eStageID::GuideRail && player->GetStatus(ePlayerStatus::IsHaveRail));
+		bool isMining = (id != eStageID::GuideRail);
+
+		for (const auto& type : m_iconMap)
+		{
+			if (id != type.first) continue;
+
+			// idとタイプが一致するなら
+			if (isGuide) m_iconBuffer.insert(make_pair(m_texMap.at(type.second).layerIndex, type.second));
+			if (isMining) m_iconBuffer.insert(make_pair(m_texMap.at(type.second).layerIndex, type.second));
+		}
+	}
+
+	// アイコンをスプライトに設定
+	void ActionGuide::UpdateIconSet()
+	{
+		// 優先度インデックス配列の設定
+		vector<int> indices;
+		size_t buffSize = m_iconBuffer.size();
+		for (const auto& buff : m_iconBuffer)
+		{
+			// アクションアイコンと一致したら追加
+			if (m_texMap.find(buff.second) != m_texMap.end())
 			{
-				if (!player->GetStatus(ePlayerStatus::IsHaveRail))
+				indices.push_back(m_texMap.at(buff.second).layerIndex);
+			}
+		}
+
+		// レイヤーのソート
+		sort(indices.begin(), indices.end());
+		if (indices.empty()) return;
+
+		// 表示数に応じてアイコンの設定方法を変更
+		int loopNum = 0;
+		for (auto& sprite : m_spriteMap)
+		{
+			bool isCenterPivot = (sprite.pivot == eIconPivot::Center); // センターかどうか
+			bool isNotBalloon = (sprite.type != eSpriteType::Balloon); // 吹き出しじゃないか
+
+			// 表示数が１ならセンターに、複数ならセンター以外に
+			if ((buffSize == 1 && isCenterPivot) || (buffSize > 1 && !isCenterPivot))
+			{
+				if (isNotBalloon)
 				{
-					if (player->GetCraftPosshible())
-					{
-						m_iconType = eActionIcon::Craft;
-					}
-					else
-					{
-						m_iconType = eActionIcon::None;
-					}
+					auto textureIter = (buffSize == 1) ? m_iconBuffer.begin()->second : std::next(m_iconBuffer.begin(), loopNum)->second;
+					sprite.sprite.lock()->SetTexture(m_texMap.at(textureIter).textureStr);
+					loopNum++;
+				}
+				sprite.active = true;
+			}
+		}
+	}
+
+	// アイコン描画更新
+	void ActionGuide::UpdateIconDraw()
+	{
+		for (auto& sprite : m_spriteMap)
+		{
+			// アクティブなら
+			if (sprite.active)
+			{
+				//複数なら
+				if (m_iconBuffer.size() > 1)
+				{
+					Vec3 diss = (sprite.pivot != eIconPivot::Left) ? Vec3(m_distanceX, 0.0f, 0.0f) : Vec3(-m_distanceX, 0.0f, 0.0f);
+					sprite.sprite.lock()->SetPosition(m_position - diss + m_typePos.at(sprite.type));
+				}
+				else // 1つなら
+				{
+					sprite.sprite.lock()->SetPosition(m_position + m_typePos.at(sprite.type));
 				}
 			}
+
+			// 描画真偽設定
+			sprite.sprite.lock()->SetDrawActive(sprite.active);
 		}
 	}
 }
--- Project/GameSources/ActionGuide.h
+++ Project/GameSources/ActionGuide.h
@@ -5,7 +5,7 @@
 */
 
 #pragma once
-#include "Billboard.h"
+#include "Sprite.h"
 #include "GamePlayer.h"
 
 namespace basecross
@@ -20,18 +20,68 @@ namespace basecross
 		Rail,	// レール
 	};
 
+	// アイコン位置タイプenum
+	enum class eIconPivot : uint8_t
+	{
+		Center,	// 中央
+		Left,	// 左
+		Right,	// 右
+	};
+
+	// スプライトタイプenum
+	enum class eSpriteType
+	{
+		Icon,	 // アイコン
+		Balloon, // 吹き出し
+	};
+
+	// テクスチャタイプ構造体
+	struct TextureType
+	{
+		wstring textureStr; // テクスチャキー 
+		uint8_t layerIndex; // 優先度レイヤー数
+
+		TextureType(wstring texStr, uint8_t index) :
+			textureStr(texStr),
+			layerIndex(index)
+		{
+		}
+	};
+
+	// スプライトタイプ構造体
+	struct SpriteType
+	{
+		eIconPivot pivot;		 // アイコンの位置
+		eSpriteType type;		 // テクスチャタイプenum
+		weak_ptr<Sprite> sprite; // スプライトのポインタ
+		bool active;			 // アクティブかの真偽
+
+		SpriteType(eIconPivot piv, eSpriteType eType, const shared_ptr<Sprite>& ptr) :
+			pivot(piv),
+			type(eType),
+			sprite(ptr)
+		{
+			active = false;
+		}
+	};
+
 	/*!
 	@brief 行動ガイドアイコン
 	*/
-	class ActionGuide : public Billboard
+	class ActionGuide : public GameObject
 	{
-		eActionIcon m_iconType;	// アイコンのタイプ
-		map<eStageID, eActionIcon> m_iconMap; // アイコンテーブル
-		map<eActionIcon, wstring> m_texMap;	// テクスチャテーブル
+		map<eStageID, eActionIcon> m_iconMap;		// アイコンテーブル
+		map<eActionIcon, TextureType> m_texMap;		// テクスチャテーブル
+		map<eSpriteType, Vec3> m_typePos;			// タイプに応じた座標
+		vector<SpriteType> m_spriteMap;				// スプライトテーブル
+		map<int, eActionIcon> m_iconBuffer;			// アイコンタイプバッファ
+		Vec3 m_position;							// 座標
 
-		const weak_ptr<GamePlayer> m_player; // プレイヤーのポインタ
-		const weak_ptr<SelectIndicator> m_indicator; // インディケーターのポインタ
-		const Vec3 m_diffPosition; // 頭上までの座標のずれ
+		const weak_ptr<GamePlayer> m_player;		// プレイヤーのポインタ
+		const weak_ptr<SelectIndicator> m_indicator;// インディケーターのポインタ
+		const Vec2 m_scale;							// スプライトのスケール
+		const Vec3 m_diffPosition;					// 頭上までの座標のずれ
+		const float m_distanceX;					// 複数表示時のX軸の余白
 
 	public:
 
@@ -45,21 +95,27 @@ namespace basecross
 			const shared_ptr<GamePlayer>& playerPtr,
 			const shared_ptr<SelectIndicator>& indicatorPtr
 		) :
-			Billboard(stagePtr, L"WHITE_TX", Vec2(1.0f), Vec3(0.0f)),
+			GameObject(stagePtr),
 			m_player(playerPtr),
 			m_indicator(indicatorPtr),
-			m_diffPosition(Vec3(0.0f, 2.0f, 0.5f))
+			m_scale(Vec2(50.0f)),
+			m_diffPosition(Vec3(0.0f, 2.0f, 0.5f)),
+			m_distanceX(30.0f)
 		{
-			m_iconType = eActionIcon::None;
+			// アイコンタイプに応じた座標テーブル
+			m_typePos.insert(make_pair(eSpriteType::Icon, Vec3(0.0f)));
+			m_typePos.insert(make_pair(eSpriteType::Balloon, Vec3(0.0f, 0.0f, 0.1f)));
 
+			// ステージIDに応じたアイコンテーブル
 			m_iconMap.insert(make_pair(eStageID::Rock, eActionIcon::Pick));
 			m_iconMap.insert(make_pair(eStageID::Tree, eActionIcon::Axe));
 			m_iconMap.insert(make_pair(eStageID::GuideRail, eActionIcon::Rail));
 
-			m_texMap.insert(make_pair(eActionIcon::Pick, L"ICON_PICK_TX"));
-			m_texMap.insert(make_pair(eActionIcon::Axe, L"ICON_AXE_TX"));
-			m_texMap.insert(make_pair(eActionIcon::Craft, L"ICON_CRAFT_TX"));
-			m_texMap.insert(make_pair(eActionIcon::Rail, L"ICON_RAIL_TX"));
+			// アイコンに応じたテクスチャと優先度テーブル
+			m_texMap.insert(make_pair(eActionIcon::Rail, TextureType(L"I_RAIL_TX", 1)));
+			m_texMap.insert(make_pair(eActionIcon::Axe, TextureType(L"I_AXE_TX", 2)));
+			m_texMap.insert(make_pair(eActionIcon::Pick, TextureType(L"I_PICK_TX", 2)));
+			m_texMap.insert(make_pair(eActionIcon::Craft, TextureType(L"I_CRAFT_TX", 3)));
 		}
 
 		/*!
@@ -77,6 +133,35 @@ namespace basecross
 		*/
 		void OnUpdate() override;
 
+		/*!
+		@brief 描画するかの設定関数
+		@param 描画するかの真偽
+		*/
+		void SetDrawActive(bool b) override
+		{
+			// 継承元の描画設定を送る
+			GameObject::SetDrawActive(b);
+
+			// 各スプライトに描画設定を送る
+			for (const auto& sprite : m_spriteMap)
+			{
+				sprite.sprite.lock()->SetDrawActive(b);
+			}
+		}
+
+		/*!
+		@brief アクティブにするかの設定関数
+		@param アクティブにするかの真偽
+		*/
+		void SetSpriteActive(bool b)
+		{
+			// 各スプライトに描画設定を送る
+			for (auto& sprite : m_spriteMap)
+			{
+				sprite.active = b;
+			}
+		}
+
 	private:
 
 		/*!
@@ -85,8 +170,18 @@ namespace basecross
 		void UpdatePosition();
 
 		/*!
-		@brief アイコンタイプの更新処理関数
+		@brief アイコンタイプの設定更新処理関数
 		*/
 		void UpdateIconType();
+
+		/*!
+		@brief アイコンセットの更新処理関数
+		*/
+		void UpdateIconSet();
+
+		/*!
+		@brief アイコン描画の更新処理関数
+		*/
+		void UpdateIconDraw();
 	};
 }
