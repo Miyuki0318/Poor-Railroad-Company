ec712fae3299db22bbcbe7b29c6a04e4e0d37390
Aoi Niitsuma <zeal22300055@fsjcl.jp>
2024-05-14 11:46:35 +0900

    タイトルから始まるようにした

--- Project/GameSources/Scene.cpp
+++ Project/GameSources/Scene.cpp
@@ -24,7 +24,7 @@ namespace basecross {
 
 			//自分自身にイベントを送る
 			//これにより各ステージやオブジェクトがCreate時にシーンにアクセスできる
-			PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"GameStage");
+			PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"TitleStage");
 		}
 		catch (...)
 		{
