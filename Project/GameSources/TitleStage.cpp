2e5bb94083d8b6e6bb46bfde422b7ad61ef4d68c
Aoi Niitsuma <zeal22300055@fsjcl.jp>
2024-05-16 13:43:27 +0900

    テクスチャキーのミスを修正した

--- Project/GameSources/TitleStage.cpp
+++ Project/GameSources/TitleStage.cpp
@@ -37,7 +37,7 @@ namespace basecross
 
 		wstring mediaPath = app->GetDataDirWString();
 		wstring texturePath = mediaPath + L"Textures/";
-		wstring modelPath = mediaPath + L"Models/" + L"Bilding";
+		wstring modelPath = mediaPath + L"Models/" + L"Bilding/";
 		
 		// タイトルロゴ
 		AddTextureResource(L"TITLE_LOGO", texturePath + L"TitleLogo.png");
@@ -46,12 +46,12 @@ namespace basecross
 		AddTextureResource(L"FADE_TX", texturePath + L"Blue.png");
 
 		// ボードのテクスチャ
-		AddTextureResource(L"BOARD_TX", modelPath + L"Bilding/RouteMapTexture.tga");
+		AddTextureResource(L"BOARD_TX", modelPath + L"RouteMapTexture.tga");
 
 		// マップ選択モデルのテクスチャ
-		AddTextureResource(L"EASY_TX", modelPath + L"Bilding/EasyMapTexture.tga");
-		AddTextureResource(L"NORMAL_TX", modelPath + L"Bilding/NormalMapTexture.tga");
-		AddTextureResource(L"HARD_TX", modelPath + L"Bilding/HardMapTexture.tga");
+		AddTextureResource(L"EASY_TX", modelPath + L"EasyMapTexture.tga");
+		AddTextureResource(L"NORMAL_TX", modelPath + L"NormalMapTexture.tga");
+		AddTextureResource(L"HARD_TX", modelPath + L"HardMapTexture.tga");
 
 		// マップのテクスチャ
 		AddTextureResource(L"EASYMAP_TX", texturePath + L"MapEasy.png");
