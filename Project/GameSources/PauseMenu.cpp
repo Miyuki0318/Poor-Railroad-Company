6b2da778eab3e5f851042434f59fe61620aca174
Yuha Yabuki <zeal22300050@fsgcl.jp>
2024-06-17 16:05:17 +0900

    状態ごとの処理を追加

--- Project/GameSources/PauseMenu.cpp
+++ Project/GameSources/PauseMenu.cpp
@@ -3,6 +3,8 @@
 
 namespace basecross
 {
+	using namespace MathF;
+
 	void PauseMenu::OnCreate()
 	{
 		const shared_ptr<Stage>& stagePtr = GetStage();
@@ -12,6 +14,39 @@ namespace basecross
 
 	void PauseMenu::OnUpdate()
 	{
+		StateProcess(m_state);
+	}
 
+	void PauseMenu::OnOpen()
+	{
+		m_state = State::Open;
+		m_scaleRatio = 0.0f;
+	}
+
+	void PauseMenu::OnClose()
+	{
+		m_state = State::Close;
+		m_scaleRatio = 1.0f;
+	}
+
+	void PauseMenu::StateProcess(State state)
+	{
+		m_scaleRatio = Clamp01(m_scaleRatio);
+		m_menuSprites.lock()->SetScale(Utility::Lerp(m_DefaultScale, m_AfterScale, m_scaleRatio));
+
+		if (state == State::None)
+		{
+
+		}
+		if (state == State::Open)
+		{
+			m_scaleRatio += DELTA_TIME * m_ScaleSpeed;
+			m_state = m_scaleRatio >= 1.0f ? State::None : State::Open;
+		}
+		if (state == State::Close)
+		{
+			m_scaleRatio -= DELTA_TIME * m_ScaleSpeed;
+			m_state = m_scaleRatio <= 0.0f ? State::None : State::Close;
+		}
 	}
 }
--- Project/GameSources/PauseMenu.h
+++ Project/GameSources/PauseMenu.h
@@ -5,8 +5,19 @@ namespace basecross
 {
 	class PauseMenu : public GameObject
 	{
+		enum State {
+			None,
+			Open,
+			Close
+		};
+		State m_state;
+
 		const Vec3 m_DefaultPosition;
 		const Vec2 m_DefaultScale;
+		const Vec2 m_AfterScale;
+		const float m_ScaleSpeed;
+
+		float m_scaleRatio;
 
 		weak_ptr<Sprite> m_menuSprites;
 
@@ -14,12 +25,21 @@ namespace basecross
 		PauseMenu(const shared_ptr<Stage>& stagePtr, const Vec3& position) :
 			GameObject(stagePtr),
 			m_DefaultPosition(position),
-			m_DefaultScale(Vec2(1920.0f, 1080.0f))
+			m_DefaultScale(Vec2(0.0f)),
+			m_AfterScale(Vec2(1920.0f, 1080.0f)),
+			m_ScaleSpeed(3.0f),
+			m_state(State::None),
+			m_scaleRatio(0.0f)
 		{
 		}
 		~PauseMenu() {}
 
 		void OnCreate() override;
 		void OnUpdate() override;
+
+		void OnOpen();
+		void OnClose();
+
+		void StateProcess(State state);
 	};
 }
