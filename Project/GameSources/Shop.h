4b19d501539256a73042fe1fabf62cd178cee0f7
Aoi-sky <zeal22300110@fsgcl.jp>
2024-06-18 13:40:23 +0900

    Shop.hとShop.cppを追加

---
+++ Project/GameSources/Shop.h
@@ -0,0 +1,19 @@
+/*!
+@file GatheringObject.h
+@brief 採取できるオブジェクト
+@author 小宅碧
+*/
+
+#pragma once
+#include "TemplateObject.h"
+
+namespace basecross
+{
+	/*!
+	@brief インスタンス描画する採取オブジェクト
+	*/
+	class Shop : public TemplateObject
+	{
+
+	};
+}
