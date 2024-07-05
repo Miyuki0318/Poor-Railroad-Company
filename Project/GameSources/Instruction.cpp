474bf936941a59b803444ab43f94a443ce2e3b48
Yuha Yabuki <zeal22300050@fsgcl.jp>
2024-07-05 13:46:56 +0900

    Instructionファイルを追加

---
+++ Project/GameSources/Instruction.cpp
@@ -0,0 +1,12 @@
+/*!
+@file Instruction.cpp
+@brief ‘€ì•û–@
+@prod –î—I—t
+*/
+
+#include "stdafx.h"
+#include "Instruction.h"
+
+namespace basecross
+{
+}
---
+++ Project/GameSources/Instruction.h
@@ -0,0 +1,17 @@
+/*!
+@file Instruction.h
+@brief ‘€ì•û–@
+@prod –î—I—t
+*/
+
+#pragma once
+#include "stdafx.h"
+#include "Sprite.h"
+
+namespace basecross
+{
+	class Instruction : public GameObject
+	{
+
+	};
+}
