897a097308eceb671407c5b4c72496490b82d929
Aoi Niitsuma <zeal22300055@fsjcl.jp>
2024-07-10 12:21:58 +0900

    ムービーステージ実装途中

--- Project/GameSources/MovieStage.cpp
+++ Project/GameSources/MovieStage.cpp
@@ -5,8 +5,50 @@
 */
 
 #include "stdafx.h"
+#include "MovieStage.h"
+#include "Scene.h"
 #include "Input.h"
 
 namespace basecross
 {
+	// ステージ移行
+	void LeaveMovieStage::ChangeStage()
+	{
+		const auto& app = App::GetApp();
+
+		m_timer += app->GetElapsedTime();
+
+		if (m_timer >= m_fadeTime)
+		{
+			m_timer = m_fadeTime;
+			PostEvent(0.0f, GetThis<ObjectInterface>(), app->GetScene<Scene>(), L"TitleStage");
+		}
+	}
+
+	// Bボタンを押した時の処理
+	void LeaveMovieStage::ButtonPush()
+	{
+		m_isPushButton = true;
+	}
+
+	void LeaveMovieStage::OnCreate()
+	{
+		MovieStage::OnCreate();
+
+		wstring dataPath;
+		App::GetApp()->GetAssetsDirectory(dataPath);
+
+		wstring moviePath = dataPath + L"";
+		SetMovieFileName(moviePath);
+
+		Play();
+	}
+
+	void LeaveMovieStage::OnUpdate()
+	{
+		if (Input::GetPush())
+		{
+			ButtonPush();
+		}
+	}
 }
--- Project/GameSources/MovieStage.h
+++ Project/GameSources/MovieStage.h
@@ -11,6 +11,33 @@ namespace basecross
 {
 	class LeaveMovieStage : public MovieStage
 	{
-		
+	private:
+		const float m_fadeTime;
+
+		float m_timer;
+
+		bool m_isPushButton;
+
+		/*
+		@brief ボタンを押した時の処理
+		*/
+		void ButtonPush();
+
+		/*
+		@brief ステージ移行
+		*/
+		void ChangeStage();
+
+	public :
+		LeaveMovieStage() :
+			MovieStage(),
+			m_fadeTime(1.0f),
+			m_timer(0.0f),
+			m_isPushButton(false)
+		{
+		}
+
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
 	};
 }
