8079fedc1a96705ae3ac631e3dab4a7a35cb4b65
Aoi Niitsuma <zeal22300055@fsjcl.jp>
2024-07-10 11:53:48 +0900

    ムービーを流すように新しいクラスを作成した

---
+++ Project/GameSources/MovieStage.cpp
@@ -0,0 +1,12 @@
+/*!
+@file MovieStage.cpp
+@brief タイトルステージの実体
+@author 新妻葵
+*/
+
+#include "stdafx.h"
+#include "Input.h"
+
+namespace basecross
+{
+}
---
+++ Project/GameSources/MovieStage.h
@@ -0,0 +1,16 @@
+/*!
+@file MovieStage.h
+@brief ムービーステージ
+@author 新妻葵
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross
+{
+	class LeaveMovieStage : public MovieStage
+	{
+		
+	};
+}
