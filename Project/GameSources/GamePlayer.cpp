eb253f13032b2357397256490e2b82502dd6b467
Yuha Yabuki <zeal22300050@fsgcl.jp>
2024-07-12 14:02:06 +0900

    フェードイン中に動けないようにした

--- Project/GameSources/GamePlayer.cpp
+++ Project/GameSources/GamePlayer.cpp
@@ -48,7 +48,7 @@ namespace basecross
 	{
 		// ポーズ中またはスクロール中なら無視
 		const auto& prog = GetTypeStage<GameStage>()->GetGameProgress();
-		bool isPause = ORS(prog, eGameProgress::Pause, eGameProgress::Opening);
+		bool isPause = ORS(prog, eGameProgress::FadeIn, eGameProgress::Pause, eGameProgress::Opening);
 		if (isPause) return;
 
 		// ステートマシンの更新処理を送る
