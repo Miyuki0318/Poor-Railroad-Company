a6e3b6ed63e0ea038a8834e579a614273b2ee5ee
Yuha Yabuki <zeal22300050@fsgcl.jp>
2024-07-16 10:38:25 +0900

    マージするとバグるので直接記述(2)

--- Project/GameSources/MainCamera.h
+++ Project/GameSources/MainCamera.h
@@ -10,6 +10,7 @@
 namespace basecross {
 	class MainCamera : public Camera {
 		const Vec3 m_MaxEye;		// カメラの最大移動距離
+		const float m_ScrollSpeed;	// スクロールスピード
 		const float m_ZoomRatioC;	// 固定状態時のズーム割合
 
 		weak_ptr<GameObject> m_targetObject; // 目標となるオブジェクト
@@ -18,12 +19,14 @@ namespace basecross {
 		Vec3 m_targetPos;	// ターゲットの位置
 		Vec3 m_zoomEye;		// ズーム後の位置
 		Vec3 m_currentEye;	// ズームのスタート位置
+		Vec3 m_defScrollEye;// カメラの初期位置(スクロール前)
 		Vec3 m_initialEye;	// カメラの初期位置
 		Vec3 m_initialAt;	// カメラの初期注視点
 
 		float m_zoomAtY;	// ズーム後の注視点
 		float m_zoomRatio;	// ズームの割合
 		float m_zoomSpeed;	// ズーム速度
+		float m_scrollRatio; // スクロール割合
 
 	public:
 		enum State {
@@ -32,6 +35,7 @@ namespace basecross {
 			ZoomIn,	// ズームイン
 			ZoomOut, // ズームアウト
 			ZoomedIn, // ズームイン後
+			Scroll,	// スクロール
 		};
 		State m_cameraState;	// カメラの現在の状態
 		const State m_DefaultState;	// カメラの初期状態
@@ -43,16 +47,41 @@ namespace basecross {
 		MainCamera(State cameraState, const Vec3& eyePos, const Vec3& atPos) : Camera(),
 			m_cameraState(cameraState),
 			m_DefaultState(cameraState),
+			m_defScrollEye(eyePos),
 			m_targetPos(Vec3(0.0f)),
-			m_initialEye(eyePos),
+			m_initialEye(m_defScrollEye),
 			m_initialAt(atPos),
 			m_MaxEye(Vec3(400.0f, 20.0f, -22.0f)),
 			m_currentEye(0.0f),
 			m_ZoomRatioC(0.6f),
+			m_ScrollSpeed(0.3f),
 			m_zoomEye(Vec3(0.0f)),
 			m_zoomAtY(0.0f),
 			m_zoomRatio(0.0f),
-			m_zoomSpeed(0.8f)
+			m_zoomSpeed(0.8f),
+			m_scrollRatio(0.0f)
+		{
+		}
+		/// <summary>
+		/// カメラのコンストラクタ
+		/// </summary>
+		/// <param name="cameraState">カメラの初期状態</param>
+		MainCamera(State cameraState, const Vec3& defEyePos, const Vec3& startEyePos, const Vec3& atPos) : Camera(),
+			m_cameraState(cameraState),
+			m_DefaultState(cameraState),
+			m_defScrollEye(defEyePos),
+			m_targetPos(Vec3(0.0f)),
+			m_initialEye(startEyePos),
+			m_initialAt(atPos),
+			m_MaxEye(Vec3(400.0f, 20.0f, -22.0f)),
+			m_currentEye(0.0f),
+			m_ZoomRatioC(0.6f),
+			m_ScrollSpeed(0.3f),
+			m_zoomEye(Vec3(0.0f)),
+			m_zoomAtY(0.0f),
+			m_zoomRatio(0.0f),
+			m_zoomSpeed(0.8f),
+			m_scrollRatio(0.0f)
 		{
 		}
 		~MainCamera() {}
@@ -76,11 +105,17 @@ namespace basecross {
 		void ZoomProcess();
 
 		/// <summary>
+		/// スクロール処理
+		/// </summary>
+		void ScrollProcess();
+
+		/// <summary>
 		/// カメラのリセット処理
 		/// </summary>
-		/// <param name="eyePos">位置</param>
-		/// <param name="atPos">注視点</param>
-		void ResetCamera(Vec3 eyePos, Vec3 atPos);
+		/// <param name="defEyePos">スクロール前の位置</param>
+		/// <param name="startEyePos">プレイスタート時の位置</param>
+		/// <param name="atPos">ゲーム中の注視点</param>
+		void ResetCamera(Vec3 defEyePos, Vec3 startEyePos, Vec3 atPos);
 
 		/// <summary>
 		/// ズーム処理スタート時に呼び出す処理
@@ -128,6 +163,14 @@ namespace basecross {
 			m_cameraState = ZoomOut;
 		}
 
+		/// <summary>
+		/// スクロール開始処理
+		/// </summary>
+		void ScrollStart()
+		{
+			m_cameraState = Scroll;
+		}
+
 		// カメラが追尾するオブジェクトを取得する関数
 		shared_ptr<GameObject> GetTargetObject() const {
 			if (!m_targetObject.expired()) {
@@ -138,6 +181,15 @@ namespace basecross {
 		// カメラが追尾するオブジェクトを設定する関数
 		void SetTargetObject(const shared_ptr<GameObject>& Obj) {
 			m_targetObject = Obj;
+		}
+
+		/// <summary>
+		/// スクロールが終了しているかどうか
+		/// </summary>
+		/// <returns>終了していたらtrue</returns>
+		bool GetScrollEnd() const
+		{
+			return m_scrollRatio >= 1.0f;
 		}
 	};
 }
